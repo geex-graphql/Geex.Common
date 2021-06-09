c30a9aac6b2362f6cc7d3eff77ce669326cb5c7b
Lulus <snys98@outlook.com>
2021-06-09 22:10:33 +0800

    initial commit

---
+++ Abstractions/BusinessException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Geex.Common.Abstractions
+{
+    public class BusinessException : Exception
+    {
+        public LogLevel LogLevel { get; }
+        public string LogMessage { get; }
+        public BusinessException(GeexExceptionType exceptionType, Exception? innerException = default, string? message = default) : base(message, innerException)
+        {
+            ExceptionName = exceptionType.Name;
+            ExceptionCode = exceptionType.Value;
+            LogMessage = message ?? exceptionType.DefaultLogMessage;
+            LogLevel = exceptionType.LogLevel;
+        }
+
+        public string ExceptionCode { get; set; }
+
+        public string ExceptionName { get; set; }
+    }
+
+    /// <summary>
+    /// inherit this enumeration to customise your own business exceptions
+    /// </summary>
+    public abstract class GeexExceptionType : Enumeration<GeexExceptionType, string>
+    {
+        protected GeexExceptionType(string name, string code, string defaultLogMessage, LogLevel logLevel = LogLevel.Warning) : base(name, code)
+        {
+            DefaultLogMessage = defaultLogMessage;
+            LogLevel = logLevel;
+        }
+
+        public string DefaultLogMessage { get; }
+        public LogLevel LogLevel { get; }
+
+    }
+}
---
+++ Abstractions/GeexModule.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HotChocolate.Execution.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+using Volo.Abp.Modularity;
+
+namespace Geex.Common.Abstractions
+{
+    public abstract class GeexModule<T> : GeexModule where T : GeexModule
+    {
+
+    }
+    public class GeexModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            base.ConfigureServices(context);
+            context.Services.Add(new ServiceDescriptor(this.GetType(), this));
+        }
+
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            base.OnApplicationInitialization(context);
+        }
+
+        public override void OnApplicationShutdown(ApplicationShutdownContext context)
+        {
+            base.OnApplicationShutdown(context);
+        }
+
+        public override void OnPostApplicationInitialization(ApplicationInitializationContext context)
+        {
+            base.OnPostApplicationInitialization(context);
+        }
+
+        public override void OnPreApplicationInitialization(ApplicationInitializationContext context)
+        {
+            base.OnPreApplicationInitialization(context);
+        }
+
+        public override void PostConfigureServices(ServiceConfigurationContext context)
+        {
+            base.PostConfigureServices(context);
+        }
+
+        public override void PreConfigureServices(ServiceConfigurationContext context)
+        {
+            base.PreConfigureServices(context);
+        }
+
+        public static HashSet<Assembly> KnownAssembly { get; } = new HashSet<Assembly>();
+    }
+
+    public abstract class GeexEntryModule<T> : GeexModule<T> where T : GeexModule
+    {
+        public IRequestExecutorBuilder SchemaBuilder => this.ServiceConfigurationContext.Services.GetSingletonInstance<IRequestExecutorBuilder>();
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.GetSingletonInstance<IRequestExecutorBuilder>().AddModuleTypes(this.GetType());
+            base.ConfigureServices(context);
+        }
+    }
+}
